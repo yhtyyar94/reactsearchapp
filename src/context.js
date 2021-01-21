809fe6ab58ad49b368a24cbcc482a8130246e800
blackeagle94 <blackeagle4894@gmail.com>
2021-01-22 01:17:33 +0200

    added spinner from brad's github repo

--- src/context.js
+++ src/context.js
@@ -6,16 +6,16 @@ const Context = React.createContext()
 export class Provider extends Component {
 
     state = {
-        track_list: [
-            {track: {tracl_name: 'abc'}},
-            {track: {tracl_name: '123'}}
-        ],
+        track_list: [],
         heading: 'Top 10 Tracks'
     }
 
     componentDidMount() {
-        axios.get(``)
-        .then(res => console.log(res.dat))
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=a7c153ed392fc137cb5af3277d01dc9a`)
+        .then(res => {
+            //console.log(res.data)
+            this.setState({...this.state, track_list: res.data.message.body.track_list})
+        })
         .catch(err => console.log(err))
     }
     
