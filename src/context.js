dbb604f88c87bc1387daff7469e53a0642a1c8b0
blackeagle94 <blackeagle4894@gmail.com>
2021-01-22 01:44:46 +0200

    styled incoming data from api and displayed

--- src/context.js
+++ src/context.js
@@ -13,7 +13,7 @@ export class Provider extends Component {
     componentDidMount() {
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=a7c153ed392fc137cb5af3277d01dc9a`)
         .then(res => {
-            //console.log(res.data)
+            console.log(res.data)
             this.setState({...this.state, track_list: res.data.message.body.track_list})
         })
         .catch(err => console.log(err))
