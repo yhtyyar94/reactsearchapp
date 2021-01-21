35088b7b7bccbde2e3546cce21d51ececd5f6085
blackeagle94 <blackeagle4894@gmail.com>
2021-01-21 23:54:30 +0200

    react-router-dom installed and implemented to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-  Hello
-    </div>
-  );
+	return (
+		<Router>
+			<React.Fragment>
+				<NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Index}/>
+          </Switch>
+        </div>
+			</React.Fragment>
+		</Router>
+	);
 }
 
 export default App;
