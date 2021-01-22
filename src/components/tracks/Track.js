0096d6e62ae9e3d1e0d0299baabe977a04792ed8
blackeagle94 <blackeagle4894@gmail.com>
2021-01-22 16:11:58 +0200

    added white spaces btwn icons and texts

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -8,12 +8,12 @@ const Track = ({ track }) => {
                 <div className="card-body">
                     <h5>{track.artist_name}</h5>
                     <p className="card-text">
-                        <strong><i className="fas fa-play"></i>Track</strong>: {track.track_name}
+                        <strong><i className="fas fa-play"></i> Track</strong>: {track.track_name}
                         <br />
-                        <strong><i className="fas fa-compact-disc"></i>Album</strong>: {track.album_name}
+                        <strong><i className="fas fa-compact-disc"></i> Album</strong>: {track.album_name}
                     </p>
                     <Link to={`lyrics/track/${track.track_id}`} className="btn btn-dark d-md-block">
-                        <i className="fas fa-chevron-right"></i>View Lyrics
+                        <i className="fas fa-chevron-right"></i> View Lyrics
                     </Link>
                 </div>
             </div>
